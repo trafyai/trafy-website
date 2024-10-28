baf04b0a6e560a512d818288b2dc20d1241dc89d
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-10-28 13:31:05 +0530

    h

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -45,10 +45,11 @@ export default function RootLayout({ children }) {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '3787797684869991');
+          fbq('init', '534717322824766');
           fbq('track', 'PageView');`
         }}
       />
+
       {/* <noscript><img height="1" width="1" style="display:none"
       src="https://www.facebook.com/tr?id=3787797684869991&ev=PageView&noscript=1"
       /></noscript> */}
