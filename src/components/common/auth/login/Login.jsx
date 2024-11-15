55a654a74c4cb923a1fe34054c2dd4601da99d71
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-11-15 10:49:41 +0530

    hi

--- src/components/common/auth/login/Login.jsx
+++ src/components/common/auth/login/Login.jsx
@@ -89,6 +89,7 @@ const Login = () => {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
+                    
                 },
                 body: JSON.stringify({ idToken: idtoken }),
             });
